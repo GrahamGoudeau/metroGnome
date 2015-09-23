be8faf53d594f54622c8b4e9b96176d42ca94e1d
Graham Goudeau <grahamgoudeau@gmail.com>
2015-09-23 17:00:53 -0400

    Metronome clicks based on input field

--- static/js/sketch.js
+++ static/js/sketch.js
@@ -1,29 +1,69 @@
 var input,
-    analyzer;
+    analyzer,
+    click,
+    bpm;
+
+
+function preload() {
+    click = loadSound('static/media/clickv1.mp3');
+}
 
 function setup() {
-    createCanvas(710, 200);
+    var canvas = createCanvas(710, 200);
+
+    canvas.parent('canvas');
+
+    bpm = document.getElementById('bpm').value;
+
+    frameRate(bpm / 60);
     background(255);
 
     // Create an Audio input
-    input = new p5.AudioIn();
+    //input = new p5.AudioIn();
 
-    input.start();
+    //input.start();
 }
 
+function draw() {
+    bpm = document.getElementById('bpm').value;
+    console.log("New bpm:");
+    console.log(bpm / 60);
+    frameRate(bpm / 60);
+    ellipse(width / 2, height / 2, 70, 70);
+    click.play();
+}
+
+/*
 function draw() {
     // Get the overall volume (between 0 and 1.0)
     var volume = input.getLevel(),
         threshold = 0.1,
         y = map(volume, 0, 1, height, 0),
-        ythreshold = map(threshold, 0, 1, height, 0);
+        ythreshold = map(threshold, 0, 1, height, 0),
+        total,
+        d = new Date(),
+        mills = d.getTime();
 
+    click.play();
+
+    dataSet.push(volume);
+    dataPoints += 1;
+    if (dataPoints % 30 === 0) {
+        total = dataSet.reduce(function (a, b) {
+            return a + b;
+        });
+
+        total = total / 30;
+        console.log(total);
+        dataPoints = 0;
+        dataSet = [];
+    }
     // If the volume > 0.1,  a rect is drawn at a random location.
     // The louder the volume, the larger the rectangle.
     if (volume > threshold) {
         stroke(0);
         fill(0, 100);
-        rect(random(40, width), random(height), volume * 50, volume * 50);
+        //rect(random(40, width), random(height), volume * 50, volume * 50);
     }
 
     // Graph the overall potential volume, w/ a line at the threshold
@@ -37,4 +77,4 @@ function draw() {
     stroke(0);
     line(0, ythreshold, 19, ythreshold);
 }
-
+*/
