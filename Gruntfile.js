f811ce5e6e713540398b394eee7ee8d729d6bc1d
Graham Goudeau <grahamgoudeau@gmail.com>
2015-09-23 00:11:27 -0400

    Downloaded p5 library, have example running

--- Gruntfile.js
+++ Gruntfile.js
@@ -25,7 +25,7 @@ module.exports = function(grunt) {
             src: 'static/js/*.js',
             options: {
                 verbose: true,
-                preset: 'jquery',
+                preset: 'crockford',
                 requireCurlyBraces: ['if']
             }
         }
--- static/js/test.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
- var x = 4;
- y = 2;
- x = y + z;
- };module.exports = 54;
- module.exports = {};
-//.hi
